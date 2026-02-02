eadf562495d29abb733949affbbe977a3a7322a1
yifei <5695274+jinyifeiooo@user.noreply.gitee.com>
2026-02-02 15:04:27 +0800

    update dependencies, add user-related classes and configurations, and implement user authentication features第三用户端开发登录注册基本功能

--- src/main/java/com/example/yuaicoder/YuaicoderApplication.java
+++ src/main/java/com/example/yuaicoder/YuaicoderApplication.java
@@ -1,9 +1,11 @@
 package com.example.yuaicoder;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.yuaicoder.mapper")
 public class YuaicoderApplication {
 
 	public static void main(String[] args) {
